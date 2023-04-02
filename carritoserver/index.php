48ba841dbed72031e059f827096d4c60e9ea9f7a
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-02 13:04:47 -0500

    Update index.php

--- carritoserver/index.php
+++ carritoserver/index.php
@@ -6,31 +6,27 @@ include 'conexion.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Control de Carro</title>
-	 <link rel="stylesheet" href="estilos.css">
+	<title>Control de Carrito</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
-	<h1>Control de Carro</h1>
-	
-	<div id="botones">
-		<button id="izquierda">Izquierda</button>
-		<button id="arriba">Arriba</button>
-		<button id="abajo">Abajo</button>
-		<button id="derecha">Derecha</button>
+	<div class="container">
+		<div class="controls">
+			<h2>Controles del Carrito</h2>
+			<button id="left-btn">Izquierda</button>
+			<button id="forward-btn">Adelante</button>
+			<button id="backward-btn">Atras</button>
+			<button id="right-btn">Derecha</button>
+			<button id="auto-mode-btn">Modo Automático</button>
+		</div>
+		<div class="info">
+			<h2>Información del Carrito</h2>
+			<p>Distancia Recorrida: <span id="distance">0</span> metros</p>
+			<p>Dirección: <span id="direction">Norte</span></p>
+			<p>Último Color Leído: <span id="color">Rojo</span></p>
+			<p>Modo: <span id="mode">Manual</span></p>
+		</div>
 	</div>
-
-	<div id="informacion">
-		<h2>Información del Carro</h2>
-		<p><strong>Distancia:</strong> <span id="distancia">0</span> m</p>
-		<p><strong>Dirección:</strong> <span id="direccion">Norte</span></p>
-		<p><strong>Último Color Leído:</strong> <span id="color">Rojo</span></p>
-		<p><strong>Modo:</strong> <span id="modo">Manual</span></p>
-	</div>
-
-	<div id="cambio-modo">
-		<button id="cambiar-modo">Cambiar Modo</button>
-	</div>
-
-	<script src="script.js"></script>
+	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
