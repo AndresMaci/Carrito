c66fbc2ed5dc11b234b9aa8639dc132d85585b9c
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-02 13:06:28 -0500

    Update script.js

--- carritoserver/script.js
+++ carritoserver/script.js
@@ -1,88 +1,71 @@
-// Obtenemos los elementos de la página
-const botonArriba = document.getElementById("boton-arriba");
-const botonAbajo = document.getElementById("boton-abajo");
-const botonIzquierda = document.getElementById("boton-izquierda");
-const botonDerecha = document.getElementById("boton-derecha");
-const botonCambioModo = document.getElementById("boton-cambio-modo");
-const distanciaElemento = document.getElementById("distancia");
-const direccionElemento = document.getElementById("direccion");
-const ultimoColorElemento = document.getElementById("ultimo-color");
-const modoElemento = document.getElementById("modo");
+const leftBtn = document.getElementById('left-btn');
+const forwardBtn = document.getElementById('forward-btn');
+const backwardBtn = document.getElementById('backward-btn');
+const rightBtn = document.getElementById('right-btn');
+const autoModeBtn = document.getElementById('auto-mode-btn');
+const distanceEl = document.getElementById('distance');
+const directionEl = document.getElementById('direction');
+const colorEl = document.getElementById('color');
+const modeEl = document.getElementById('mode');
 
-// Inicializamos las variables
-let distancia = 0;
-let direccion = "Norte";
-let ultimoColor = "Rojo";
-let modo = "Manual";
+// Definir variables de estado del carrito
+let distance = 0;
+let direction = 'Norte';
+let color = 'Rojo';
+let mode = 'Manual';
 
-// Función para actualizar la información del carro
-function actualizarInformacionCarro() {
-  distanciaElemento.textContent = distancia;
-  direccionElemento.textContent = direccion;
-  ultimoColorElemento.textContent = ultimoColor;
-  modoElemento.textContent = modo;
+// Funciones para manejar los eventos de los botones
+function moveLeft() {
+	if (mode === 'Manual') {
+		console.log('Moviendo hacia la izquierda');
+	}
 }
 
-// Función para mover el carro hacia arriba
-function moverArriba() {
-  if (modo === "Manual") {
-    direccion = "Norte";
-    distancia += 10;
-    actualizarInformacionCarro();
-  }
+function moveForward() {
+	if (mode === 'Manual') {
+		console.log('Moviendo hacia adelante');
+		distance += 1;
+		distanceEl.innerText = distance;
+	}
 }
 
-// Función para mover el carro hacia abajo
-function moverAbajo() {
-  if (modo === "Manual") {
-    direccion = "Sur";
-    distancia -= 10;
-    actualizarInformacionCarro();
-  }
+function moveBackward() {
+if (mode === 'Manual') {
+console.log('Moviendo hacia atrás');
+distance -= 1;
+distanceEl.innerText = distance;
+}
 }
 
-// Función para mover el carro hacia la izquierda
-function moverIzquierda() {
-  if (modo === "Manual") {
-    direccion = "Oeste";
-    distancia += 10;
-    actualizarInformacionCarro();
-  }
+function moveRight() {
+if (mode === 'Manual') {
+console.log('Moviendo hacia la derecha');
+}
 }
 
-// Función para mover el carro hacia la derecha
-function moverDerecha() {
-  if (modo === "Manual") {
-    direccion = "Este";
-    distancia -= 10;
-    actualizarInformacionCarro();
-  }
+function toggleAutoMode() {
+if (mode === 'Manual') {
+console.log('Cambiando a modo automático');
+mode = 'Automático';
+leftBtn.disabled = true;
+forwardBtn.disabled = true;
+backwardBtn.disabled = true;
+rightBtn.disabled = true;
+modeEl.innerText = mode;
+} else {
+console.log('Cambiando a modo manual');
+mode = 'Manual';
+leftBtn.disabled = false;
+forwardBtn.disabled = false;
+backwardBtn.disabled = false;
+rightBtn.disabled = false;
+modeEl.innerText = mode;
+}
 }
 
-// Función para cambiar el modo del carro
-function cambiarModo() {
-  if (modo === "Manual") {
-    modo = "Automático";
-    botonArriba.disabled = true;
-    botonAbajo.disabled = true;
-    botonIzquierda.disabled = true;
-    botonDerecha.disabled = true;
-  } else {
-    modo = "Manual";
-    botonArriba.disabled = false;
-    botonAbajo.disabled = false;
-    botonIzquierda.disabled = false;
-    botonDerecha.disabled = false;
-  }
-  actualizarInformacionCarro();
-}
-
-// Añadimos los eventos a los botones
-botonArriba.addEventListener("click", moverArriba);
-botonAbajo.addEventListener("click", moverAbajo);
-botonIzquierda.addEventListener("click", moverIzquierda);
-botonDerecha.addEventListener("click", moverDerecha);
-botonCambioModo.addEventListener("click", cambiarModo);
-
-// Actualizamos la información del carro por primera vez
-actualizarInformacionCarro(); 
+// Asignar manejadores de eventos a los botones
+leftBtn.addEventListener('click', moveLeft);
+forwardBtn.addEventListener('click', moveForward);
+backwardBtn.addEventListener('click', moveBackward);
+rightBtn.addEventListener('click', moveRight);
+autoModeBtn.addEventListener('click', toggleAutoMode);
