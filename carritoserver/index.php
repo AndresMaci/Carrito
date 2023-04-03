e6853d2d850548300874587fa742b47ada96b168
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-03 11:54:37 -0500

    Update index.php

--- carritoserver/index.php
+++ carritoserver/index.php
@@ -15,27 +15,16 @@ $conectado= $_POST['con'];
 	<div class="container">
 		<div class="controls">
 			<h2>Controles del Carrito</h2>
-			<a href="/iz">
 			   <button id="left-btn">Izquierda</button>
-				</a>
-			<a href="/ad">
 			   <button id="forward-btn">Adelante</button>
-				</a>
-			<a href="/at">
 			   <button id="backward-btn">Atras</button>
-				</a>
-			<a href="/de">
 			   <button id="right-btn">Derecha</button>
-				</a>
-			
-				<a href="/modo">
 			   <button id="auto-mode-btn">Modo Automático</button>
-					</a>
 				
 		</div>
 		<div class="info">
 			<h2>Información del Carrito</h2>
-			<p>Carro conectado: <span id="c">Conectado</span></p>
+			<p>Carro conectado: <span id="c"><?php echo $conectado; ?></span></p>
 			<p>Distancia Recorrida: <span id="distance">0</span> metros</p>
 			<p>Dirección: <span id="direction">Norte</span></p>
 			<p>Último Color Leído: <span id="color">Rojo</span></p>
