0fd9f73d8d341843b417baf0e2a134deb873d055
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-04 20:54:57 -0500

    Update conexion.php

--- carritoserver/conexion.php
+++ carritoserver/conexion.php
@@ -7,22 +7,4 @@ $conecta = mysqli_connect($servidor,$usuario,$password,$bd);
 if($conecta->connect_error){
     die("Error al conectar".$conecta->connect_error);
 }
-
-$sql = "SHOW TABLES";
-
-// Ejecutar la consulta
-$resultado = mysqli_query($conecta, $sql);
-
-// Verificar si hay resultados
-if (mysqli_num_rows($resultado) > 0) {
-    // Mostrar los nombres de las tablas
-    while ($fila = mysqli_fetch_row($resultado)) {
-        echo $fila[0] . "<br>";
-    }
-} else {
-    echo "No se encontraron resultados.";
-}
-
-// Cerrar la conexión
-mysqli_close($conexion);
 ?>
