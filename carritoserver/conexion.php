46a06b7b0134058a150c3fceecbb48de9cec007b
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-05 11:23:38 -0500

    Update conexion.php

--- carritoserver/conexion.php
+++ carritoserver/conexion.php
@@ -7,4 +7,13 @@ $conecta = mysqli_connect($servidor,$usuario,$password,$bd);
 if($conecta->connect_error){
     die("Error al conectar".$conecta->connect_error);
 }
+
+$parametro1 = $_GET['time'];
+  $parametro2 = $_GET['direccion'];
+$parametro3 = $_GET['distancia'];
+  $parametro4 = $_GET['ulColor'];
+$parametro5 = $_GET['modo'];
+
+echo "hola ".$parametro1 .$parametro2 .$parametro3 .$parametro4 .$parametro5 ;
+
 ?>
