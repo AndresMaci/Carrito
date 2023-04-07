d3690712670ff1e3567969e215aaaa8d7a40e94a
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-07 00:19:00 -0500

    Update consultas.php

--- carritoserver/consultas.php
+++ carritoserver/consultas.php
@@ -1,2 +1,16 @@
 <?php
+
+$resultado = mysqli_query($conexion, "SELECT valor FROM tabla WHERE id = 1");
+
+if (!$resultado) {
+    die("La consulta SQL ha fallado: " . mysqli_error($conexion));
+}
+
+$fila = mysqli_fetch_assoc($resultado);
+$valor = $fila['valor'];
+
+mysqli_close($conexion);
+
+echo $valor;
+
 ?>
