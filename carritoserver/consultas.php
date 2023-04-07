2ab33b90dd74d47a1a180156a947f2afbcd9aaa2
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-07 14:29:27 -0500

    Update consultas.php

--- carritoserver/consultas.php
+++ carritoserver/consultas.php
@@ -2,15 +2,32 @@
 include 'conexion.php';
 
 
-$resultado = mysqli_query($conecta, "SELECT c FROM carrito WHERE time = 1");
+$resultado = mysqli_query($conecta, "SELECT * FROM carrito WHERE time = 1");
 
 if (!$resultado) {
     die("La consulta SQL ha fallado: " . mysqli_error($conecta));
 }
 
 $fila = mysqli_fetch_assoc($resultado);
-$valor = $fila['c'];
-$c=$valor;
+$c = $fila['c'];
+$dr = $fila['direccion'];
+$dis = $fila['distancia'];
+$cl = $fila['ulColor'];
+
+if ($dr==0){
+    $dr="Avanzando";
+}
+if ($dr==1){
+    $dr="Retrocediendo";
+}
+if ($dr==2){
+    $dr="Derecha";
+}
+if ($dr==3){
+    $dr="Iquierda";
+}
+
+
 if ($c==1){
     $c="Desconectado";
 }
@@ -18,5 +35,5 @@ if ($c==0){
     $c="Conectado";
 }
 
-echo $c;
+echo $c ." ".$dr ." ".$dis ." ".$cl;
 ?>
