b29bd26663eaa171fe905aeb46b34bd14ec42c9f
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-07 13:11:14 -0500

    Update ac.php

--- carritoserver/ac.php
+++ carritoserver/ac.php
@@ -5,7 +5,7 @@ if (isset($_POST['D'])) {
   $sql = "UPDATE carrito SET c= 1 WHERE time= 1";
 $resultado = mysqli_query($conecta, $sql);
 if (!$resultado) {
-  die("Error al actualizar la base de datos: " . mysqli_error($conexion));
+  die("Error al actualizar la base de datos: " . mysqli_error($conecta));
 }
 
 }
