d75756259e2db7d1fc2b0b3106b9ba0d30ae24fc
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-07 01:08:38 -0500

    Update script.js

--- carritoserver/script.js
+++ carritoserver/script.js
@@ -87,9 +87,7 @@ rightBtn.disabled = true;
  setInterval(function() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
             document.getElementById("c").innerHTML = this.responseText;
-        }
     };
     xmlhttp.open("GET", "consultas.php", true);
     xmlhttp.send();
