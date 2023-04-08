2f4185ac20b1356d8c2e4a185adb535fb9f2377e
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-08 02:01:20 -0500

    Update index.php

--- carritoserver/index.php
+++ carritoserver/index.php
@@ -6,6 +6,8 @@
 	
 	<title>Control de Carrito</title>
 	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-Wz/oy+D39/x/5pE1XN5r5MW5fNql51PouLe5NalzJv92yIg+up24OMJyOhX9ovr5r+O5lJl2+tZLe5Hc+2bUbg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 <body>
 	<div class="container">
