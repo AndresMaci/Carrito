603a1e371bb7b66e5da81a3f8729521cd58ce801
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-08 17:20:29 -0500

    Update index.php

--- carritoserver/index.php
+++ carritoserver/index.php
@@ -6,8 +6,6 @@
 	
 	<title>Control de Carrito</title>
 	<link rel="stylesheet" type="text/css" href="estilos.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-Wz/oy+D39/x/5pE1XN5r5MW5fNql51PouLe5NalzJv92yIg+up24OMJyOhX9ovr5r+O5lJl2+tZLe5Hc+2bUbg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
 </head>
 <body>
 	<div class="container">
@@ -19,7 +17,7 @@
         <button id="backward-btn">Atras</button>
         <button id="right-btn">Derecha</button>
 	    <button id="p">Parar</button>
-        <button id="auto-mode-btn">Modo Automático</button>
+        <button id="auto-mode-btn">Cambiar modo</button>
     </div>
 </div>
 
