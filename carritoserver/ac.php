14cda9e4c0aebfd60c3eabc64b41189190dd44e1
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-07 23:16:19 -0500

    Update ac.php

--- carritoserver/ac.php
+++ carritoserver/ac.php
@@ -1,10 +1,46 @@
 <?php
 include 'conexion.php';
 
-  $sql = "UPDATE carrito SET c= 1 WHERE time= 1";
+$op= $_GET['v'];
+$v=0;
+if ($op==1) {
+  $op="c";
+ $v=1;
+}
+if ($op==2) {
+  $op="modo";
+   $sql = "SELECT * FROM carrito WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+if (!$resultado) {
+  die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+}
+  $fila = mysqli_fetch_assoc($resultado);
+$c = $fila['modo'];
+if ($c==1)
+  $op="direccion";
+   $v=0;
+}
+if ($c==0)
+   $op="direccion";
+   $v=1;
+}
+if ($op==3) {
+   $op="direccion";
+ $v=1;
+}
+if ($op==4) {
+   $op="direccion";
+ $v=2;
+}
+if ($op==5) {
+   $op="direccion";
+ $v=3;
+}
+ $sql = "UPDATE carrito SET" .$op ."=" .$v ."WHERE time= 1";
 $resultado = mysqli_query($conecta, $sql);
 if (!$resultado) {
   die("Error al actualizar la base de datos: " . mysqli_error($conecta));
 }
 echo "ok";
+exit();
 ?>
