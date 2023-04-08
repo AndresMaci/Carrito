a8f91304bd84809d4a9490693a76c71a2baa012b
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-08 14:42:55 -0500

    Update concarrito.php

--- carritoserver/concarrito.php
+++ carritoserver/concarrito.php
@@ -1,56 +1,52 @@
 <?php
-$servidor="3.128.205.248";
-$usuario="remoto";
-$password="andresramos";
-$bd="carrito";
-$conecta = mysqli_connect($servidor,$usuario,$password,$bd);
-if($conecta->connect_error){
-    die("Error al conectar".$conecta->connect_error);
+$servidor = "3.128.205.248";
+$usuario = "remoto";
+$password = "andresramos";
+$bd = "carrito";
+$conecta = mysqli_connect($servidor, $usuario, $password, $bd);
+if ($conecta->connect_error) {
+    die("Error al conectar" . $conecta->connect_error);
 }
 $op = $_GET['op'];
 $v = $_GET['v'];
-echo $op;
- if ($op == 1) {
-        $sql = "UPDATE carrito SET c = 0 WHERE time = 1";
-        $resultado = mysqli_query($conecta, $sql);
-        if (!$resultado) {
-            die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-        }
-     echo "bien";
-    } elseif ($op == 2) {
-      $op = "modo";
-        $sql = "SELECT * FROM carrito WHERE time = 1";
-        $resultado = mysqli_query($conecta, $sql);
-        if (!$resultado) {
-            die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-        }
-        $fila = mysqli_fetch_assoc($resultado);
-        $c = $fila[$op];
-     echo $c;
-    } elseif ($op == 3) {
-          $op = "direccion";
-        $sql = "SELECT * FROM carrito WHERE time = 1";
-        $resultado = mysqli_query($conecta, $sql);
-        if (!$resultado) {
-            die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-        }
-        $fila = mysqli_fetch_assoc($resultado);
-        $c = $fila[$op];
-     echo $c;
-     
-    } elseif ($op == 4) {
-         $sql = "UPDATE carrito distancia  = $v WHERE time = 1";
-        $resultado = mysqli_query($conecta, $sql);
-        if (!$resultado) {
-            die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-        }
-    } elseif ($op == 5) {
-         $sql = "UPDATE carrito ulColor  = $v WHERE time = 1";
-        $resultado = mysqli_query($conecta, $sql);
-        if (!$resultado) {
-            die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-        }
-    } 
+if ($op == 1) {
+    $sql = "UPDATE carrito SET c = 0 WHERE time = 1";
+    $resultado = mysqli_query($conecta, $sql);
+    if (!$resultado) {
+        die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+    }
+    echo "bien";
+} elseif ($op == 2) {
+    $op = "modo";
+    $sql = "SELECT * FROM carrito WHERE time = 1";
+    $resultado = mysqli_query($conecta, $sql);
+    if (!$resultado) {
+        die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+    }
+    $fila = mysqli_fetch_assoc($resultado);
+    $c = $fila[$op];
+    echo $c;
+} elseif ($op == 3) {
+    $op = "direccion";
+    $sql = "SELECT * FROM carrito WHERE time = 1";
+    $resultado = mysqli_query($conecta, $sql);
+    if (!$resultado) {
+        die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+    }
+    $fila = mysqli_fetch_assoc($resultado);
+    $c = $fila[$op];
+    echo $c;
+} elseif ($op == 4) {
+    $sql = "UPDATE carrito SET distancia = '$v' WHERE time = 1";
+    $resultado = mysqli_query($conecta, $sql);
+    if (!$resultado) {
+        die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+    }
+} elseif ($op == 5) {
+    $sql = "UPDATE carrito SET ulColor = '$v' WHERE time = 1";
+    $resultado = mysqli_query($conecta, $sql);
+    if (!$resultado) {
+        die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+    }
 }
-
 ?>
