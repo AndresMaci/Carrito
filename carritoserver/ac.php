75ccae782d24b4d39c2565cc7dfc05464a811e69
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-08 01:24:35 -0500

    Update ac.php

--- carritoserver/ac.php
+++ carritoserver/ac.php
@@ -3,6 +3,65 @@
 if(isset($_GET['v'])) {
 include 'conexion.php';
   $op = $_GET['v'];
-echo $op;
+  $op = $_GET['v'];
+$v = 0;
+
+if ($op == 1) {
+  $sql = "UPDATE carrito SET c =1 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+} elseif ($op == 2) {
+  $op = "modo";
+  $sql = "SELECT * FROM carrito WHERE time = 1";
+  $resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+  $fila = mysqli_fetch_assoc($resultado);
+  $c = $fila[$op];
+  if ($c == 1) {
+  $sql = "UPDATE carrito SET modo =0 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+
+  } else {
+     $sql = "UPDATE carrito SET modo =1 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+  }
+} elseif ($op == 3) {
+  $sql = "UPDATE carrito SET direccion =0 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+} elseif ($op == 4) {
+   $sql = "UPDATE carrito SET direccion =1 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+} elseif ($op == 5) {
+   $sql = "UPDATE carrito SET direccion =2 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+} elseif ($op == 6) {
+  $sql = "UPDATE carrito SET direccion =3 WHERE time = 1";
+$resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
+}
+
+
+echo "ok";
 }
 ?>
