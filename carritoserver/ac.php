3b5ccc6ce2f02a3635c7c3932d7422ed06b09da9
AndresMaci <114184889+AndresMaci@users.noreply.github.com>
2023-04-08 00:10:50 -0500

    Update ac.php

--- carritoserver/ac.php
+++ carritoserver/ac.php
@@ -1,40 +1,42 @@
 <?php
 include 'conexion.php';
 
-$op= $_GET['v'];
-$v=0;
-if ($op==1) {
-  $op="c";
- $v=1;
-}
-if ($op==2) {
-  $op="modo";
-   $sql = "SELECT * FROM carrito WHERE time = 1";
-$resultado = mysqli_query($conecta, $sql);
-if (!$resultado) {
-  die("Error al actualizar la base de datos: " . mysqli_error($conecta));
-}
+$op = $_GET['v'];
+$v = 0;
+
+if ($op == 1) {
+  $op = "c";
+  $v = 1;
+} elseif ($op == 2) {
+  $op = "modo";
+  $sql = "SELECT * FROM carrito WHERE time = 1";
+  $resultado = mysqli_query($conecta, $sql);
+  if (!$resultado) {
+    die("Error al actualizar la base de datos: " . mysqli_error($conecta));
+  }
   $fila = mysqli_fetch_assoc($resultado);
-$c = $fila['modo'];
-if ($op==3)
-  $op="direccion";
-   $v=0;
+  $c = $fila[$op];
+  if ($c==1){
+    $v=0;
+  }elseif{
+     $v=1;
+  }
+} elseif ($op == 3) {
+  $op = "direccion";
+} elseif ($op == 4) {
+  $op = "direccion";
+  $v = 1;
+} elseif ($op == 5) {
+  $op = "direccion";
+  $v = 2;
+} elseif ($op == 6) {
+  $op = "direccion";
+  $v = 3;
 }
-if ($op==4)
-   $op="direccion";
-   $v=1;
-}
-if ($op==5) {
-   $op="direccion";
- $v=2;
-}
-if ($op==6) {
-   $op="direccion";
- $v=3;
-}
-$stmt = $conecta->prepare("UPDATE carrito SET :c WHERE id = :v");
-$stmt->bindParam(':c', $op);
-$stmt->bindParam(':v', $v);
+
+$sql = "UPDATE carrito SET $op = :valor WHERE time = :id";
+$stmt = $conecta->prepare($sql);
+$stmt->bindParam(':valor', $v);
 $stmt->execute();
 echo "ok";
 ?>
